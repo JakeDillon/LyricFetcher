318713304ee7abb4d130a0e445d4d770ac53ae63
Jake Dillon <jake.dillon168@gmail.com>
2018-10-23 10:33:39 -0500

    commy init files

---
+++ LyricFetcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LyricFetcher
+//
+//  Created by Jake Dillon on 10/23/18.
+//  Copyright © 2018 Jake Dillon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
